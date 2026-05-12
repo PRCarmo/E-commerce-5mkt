0c33d6fa64e947ffe435eb1c70a02177cf518bcf
Pedro Rodrigo <workingpedrr@gmail.com>
2026-05-12 16:22:00 -0300

    Remoção dos arquivos model duplicados no primeiro nível do diretório

--- 5mkt/model/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.fivemkt.marketplace.model;
-
-public enum OrderStatus {
-    PENDING, CONFIRMED, SHIPPED, DELIVERED, CANCELLED
-}
--- 5mkt/model/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.fivemkt.marketplace.model;
-
-public enum UserRole {
-    ADMIN, SELLER, CUSTOMER
-}
